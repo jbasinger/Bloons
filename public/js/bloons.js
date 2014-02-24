5556df07f22e4df840fe1ab88c4d3790d2ac2b60
Justin Basinger <justin.m.basinger@gmail.com>
2014-02-24 13:00:40 -0500

    Cleaned up a bunch of code. Added a timer component

--- public/js/bloons.js
+++ public/js/bloons.js
@@ -1,4 +1,46 @@
 
+var Entity = function(){
+	
+	var self = this;
+	
+	self.addComponent = function(component){
+		if (!component){
+			console.log("Attempted adding a null component.");
+			return;
+		}
+			
+		_.extend(self,component);
+		if (_.isFunction(component.added))
+			component.added(self);
+	}
+	
+	//This is probably terrible. =)
+	self.removeComponent = function(component){
+		
+		if (!component){
+			console.log("Attempted removing a null component.");
+			return;
+		}
+		
+		_.each(_.keys(component),function(k){
+			if (self[k])
+				delete self[k];
+		});
+		if (_.isFunction(component.removed))
+			component.removed(self);
+	}
+	
+	return self;
+	
+}
+
+var Component = function(){
+	
+	var self = this;
+	var added = function(entity){};
+	
+}
+
 createjs.SceneManager = (function(){
 	
 	//Scenes should have names so we can show them
@@ -65,6 +107,8 @@ var game_state = _.extend({},game_init_state);
 var bloons = [];
 var $canvas = $("#game");
 var stage = new createjs.Stage($canvas[0]);
+var queue = new createjs.LoadQueue(true);
+var timer = new Entity();
 
 $(function(){
 	
@@ -73,8 +117,7 @@ $(function(){
 	var menu = new createjs.Container().set({name: "menu"});
 	var board = new createjs.Container().set({name: "board"});
 	
-	configMenu(menu, $canvas);
-	configBoard(board, $canvas);
+	
 	
 	createjs.SceneManager.addScene(menu, function(){
 		//Shown callback
@@ -82,26 +125,14 @@ $(function(){
 	}, function(){
 		stage.enableMouseOver(0);
 	});
-	createjs.SceneManager.addScene(board, function(){
-		//Shown callback
-		_.each(bloons,function(b){
-			b.tween.setPaused(false);
-		});
-	}, function(){
-		//hidden callback
-		_.each(bloons,function(b){
-			b.tween.setPaused(true);
-		});
-	});
+	
+	createjs.SceneManager.addScene(board);
 	
 	window.addEventListener("keydown", function(e){
-		
-		//console.log(e);
-		
 		if (e.keyCode == 27 && createjs.SceneManager.activeScene.name == "board" ){
 			createjs.SceneManager.showScene("menu",stage);
+			pauseBoard();
 		}
-
 		e.preventDefault();
 	});
 	
@@ -109,11 +140,10 @@ $(function(){
 		{src:"images/bloon.png", id:"bloon"},
 	];
 	
-	for (var i=1; i < 6; i++)
+	for (var i=1; i < 6; i++){
 		manifest.push({src:"sounds/p" + i + ".wav", id:"p" + i});
+	}
 	
-	var queue = new createjs.LoadQueue(true);
-
 	queue.installPlugin(createjs.Sound);
 	
 	queue.on("error", function(evt){
@@ -122,17 +152,8 @@ $(function(){
 	
 	queue.on("complete", function(evt){
 			
-		for (var i=0; i < MAX_BLOONS; i++){
-			
-			var bloon = new Bloon(BLOON_TYPE.NORMAL, $canvas, queue.getResult("bloon"), board);
-
-			//We need to DRY up the creation and deletion of bloons.
-			//I want it to be contained in the object but decoupled from the board...
-			//events? bloonFactory?
-			board.addChild(bloon);
-			bloons.push(bloon);
-
-		}
+		configMenu(menu, $canvas);
+		configBoard(board, $canvas);
 		
 		createjs.SceneManager.showScene("menu", stage);
 		
@@ -147,77 +168,43 @@ $(function(){
 	
 });
 
+//Misc functions
 function rand(min,max){ //Inclusive
 	min = min || 0;
 	max = max || 1;
 	return Math.random() * (max - min) + min;
 }
 
+function showBoard(){
+	createjs.SceneManager.showScene("board", stage);
+	_.each(bloons, function(b){
+		b.SwayHorizontal.setPaused(false);
+		b.AnimateDown.setPaused(false);
+	});
+	timer.timers["timer"].paused = false;
+}
+
+function pauseBoard(){
+	_.each(bloons, function(b){
+		b.SwayHorizontal.setPaused(true);
+		b.AnimateDown.setPaused(true);
+	});
+	timer.timers["timer"].paused = true;
+}
+
+// Game specific functions
 function configMenu(menuContainer, $canvas){
-	
-	var title = new createjs.Text("BLOONS!", "bold 90px Arial", "#00FFFF").set({x: $canvas.width()/2, y: $canvas.height()/6.28});
-	title.set({regX: title.getMeasuredWidth()/2, regY: title.getMeasuredHeight()/2});
-	
-	var playMode = new createjs.Text("TIME MODE", "64px Arial", "#FF0000").set({x: $canvas.width()/2, y: title.y + 150});
-	playMode.set({regX: playMode.getMeasuredWidth()/2, regY: playMode.getMeasuredHeight()/2});
-	
-	var hit = new createjs.Shape();
-	hit.graphics.beginFill("#000").drawRect(0, 0, playMode.getMeasuredWidth(), playMode.getMeasuredHeight());
-	playMode.hitArea = hit;
-	
-	playMode.flippingOut = false;
-	
-	playMode.on("mouseover", function(evt){
-			playMode.flippingOut = true;
-	});
-	
-	playMode.on("mouseout", function(evt){
-		playMode.flippingOut = false;
-	});
-	
-	playMode.on("click", function(evt){
-		createjs.SceneManager.showScene("board", stage);
-	});
-	
-	var zenMode = new createjs.Text("ZEN MODE", "64px Arial", "#FF00FF").set({x: $canvas.width()/2, y: title.y + 250});
-	zenMode.set({regX: zenMode.getMeasuredWidth()/2, regY: zenMode.getMeasuredHeight()/2});
-	
-	//This could use some DRYing
-	var hit2 = new createjs.Shape();
-	hit2.graphics.beginFill("#000").drawRect(0, 0, zenMode.getMeasuredWidth(), zenMode.getMeasuredHeight());
-	zenMode.hitArea = hit2;
-	
-	zenMode.flippingOut = false;
-	
-	zenMode.on("mouseover", function(evt){
-			zenMode.flippingOut = true;
-	});
-	
-	zenMode.on("mouseout", function(evt){
-		zenMode.flippingOut = false;
-	});
-	
-	zenMode.on("click", function(evt){
-		//Change the mode here too.
-		createjs.SceneManager.showScene("board", stage);
-	});
-	
-	
-	createjs.Ticker.addEventListener("tick",function(evt){
-		if (playMode.flippingOut){
-			playMode.color = "#" + _.random(0,255).toString(16) + _.random(0,255).toString(16) + _.random(0,255).toString(16);
-		} else {
-			playMode.color = "#FF0000";
-		}
 		
-		if (zenMode.flippingOut){
-			zenMode.color = "#" + _.random(0,255).toString(16) + _.random(0,255).toString(16) + _.random(0,255).toString(16);
-		} else {
-			zenMode.color = "#FF0000";
-		}
-	});
+	var title = new Entity();
+	title.addComponent(new Text("BLOONS!", "bold 90px Arial", "#00FFFF"));
+	title.addComponent(new Position($canvas.width()/2, $canvas.height()/6.28, title.getMeasuredWidth()/2, title.getMeasuredHeight()/2));
 	
-	console.log(_.random(0,255).toString(16));
+	var playMode = new MenuOption("TIME MODE", "64px Arial", "#FF0000", $canvas.width()/2, title.y + 150, function(evt){
+		showBoard();
+	});
+	var zenMode = new MenuOption("ZEN MODE", "64px Arial", "#FF0000", $canvas.width()/2, title.y + 250, function(evt){
+		showBoard();
+	});
 	
 	menuContainer.addChild(title);
 	menuContainer.addChild(playMode);
@@ -227,81 +214,307 @@ function configMenu(menuContainer, $canvas){
 }
 
 function configBoard(boardContainer, $canvas){
+
+		for (var i=0; i < MAX_BLOONS; i++){
+			
+			var bloon = new Bloon();
+
+			boardContainer.addChild(bloon);
+			bloons.push(bloon);
+
+		}
+
+		timer.addComponent(new Timer("timer",{duration:60000, pause:true}, function(n,e){
+			console.log("GET TO TWERK!");
+		}));
+		
+		timer.getSeconds = function(){
+			
+			var t = this.timers["timer"];
+			
+			if (!t)
+				return 0 + "";
+			
+			return Math.round(t.duration/1000) + "";
+			
+		}
+		
+		timer.addComponent(new Text("Time Left: " + timer.getSeconds(), "32px Arial", "#00FFFF", 0, 0));
+		
+		createjs.Ticker.on("tick", function(evt){
+			timer.text = "Time Left: " + timer.getSeconds();
+		});
+		
+		boardContainer.addChild(timer);
+		
+}
+
+//Components
+var RandomColor = function(){
+	
+	var comp = new Component();
+	
+	comp.filters = [new createjs.ColorFilter(0.25,0.25,0.25,1,rand(0,255),rand(0,255),rand(0,255),0)];
+	
+	comp.added = function(e){
+		e.cache(0,0,e.width, e.height);
+	}
+	
+	return comp;
+}
+
+var Bitmap = function(image){
+	
+	var comp = new Component();
+	
+	_.extend(comp,new createjs.Bitmap(image).set({
+		height: image.height,
+		width: image.width
+	}));
+	
+	return comp;
 	
 }
 
-var Bloon = function(type, $canvas, image, stage){
+var Position = function(x,y, regX, regY){
 	
-	var width = image.width;
-	var height = image.height;
+	//TODO: Add functions here to allow centering on things
+	//this component is kind of boring
 	
-	var bloon = new createjs.Bitmap(image).set({
-		height: height,
-		width: width,
-		x: _.random(0, $canvas.width()-width),
-		y: _.random(-5*height, 0-height),
-		v: _.random(10000,15000)
-	});
+	var comp = new Component();
 	
-	bloon.filters = [new createjs.ColorFilter(0.25,0.25,0.25,1,rand(0,255),rand(0,255),rand(0,255),0)];
-	bloon.snapToPixel = true;
-	bloon.cache(0, 0, width, height);
+	comp.x = x;
+	comp.y = y;
 	
-	bloon.adios = function(){
-		stage.removeChild(this);
-		bloons = _.without(bloons, this);
-		if (bloons.length < MAX_BLOONS){
-			var newBloon = new Bloon(BLOON_TYPE.NORMAL, $canvas, image, stage);
-			bloons.push(newBloon);
-			stage.addChild(newBloon);
+	if (regX){
+		comp.regX = regX;
+	}
+	
+	if (regY){
+		comp.regY = regY;
+	}
+	
+	comp.added = function(e){
+		//console.log(e.y);
+	}
+	
+	return comp;
+}
+
+var Text = function(text, font, color){
+	
+	var comp = new Component();
+	
+	_.extend(comp, new createjs.Text(text, font, color));
+	
+	comp.added = function(e){
+		var hit = new createjs.Shape();
+		hit.graphics.beginFill("#000").drawRect(0, 0, e.getMeasuredWidth(), e.getMeasuredHeight());
+		e.hitArea = hit;
+	}
+	
+	return comp;
+	
+}
+
+var MouseOverFlipOut = function(){
+	
+	var comp = new Component();
+	comp.flippingOut = false;
+	
+	comp.added = function(e){
+		
+		e.on("mouseover", function(evt){
+			e.flippingOut = true;
+		});
+		
+		e.on("mouseout", function(evt){
+			e.flippingOut = false;
+		});
+		
+		createjs.Ticker.addEventListener("tick",function(evt){
+			if (e.flippingOut && e.color){
+				e.color = "#" + _.random(0,255).toString(16) + _.random(0,255).toString(16) + _.random(0,255).toString(16);
+			} else {
+				e.color = "#FF0000";
+			}
+		});
+
+	}
+	
+	return comp;
+	
+}
+
+var SwayHorizontal = function(dt){
+	
+	var comp = new Component();
+	
+	comp.added = function(e){
+		var wave = _.random(-5*e.width, 5*e.width);
+		var origX = e.x;
+		e.SwayHorizontal = createjs.Tween.get(e, {loop: true})
+			.to({x: e.x + wave}, dt, createjs.Ease.sineInOut)
+			.to({ x: origX }, dt, createjs.Ease.sineInOut);
+	}
+	
+	return comp;
+	
+}
+
+var AnimateDown = function(dt, height, onDone){
+
+	var comp = new Component();
+	
+	comp.added = function(e){
+		var tween = createjs.Tween.get(e).to({
+			y: height
+		}, dt, createjs.Ease.linear);
+		
+		if (_.isFunction(onDone)){
+			tween.call(onDone);
+		}
+		
+		e.AnimateDown = tween;
+		
+	}
+	
+	return comp;
+}
+
+var Removeable = function(onRemoved){
+
+	var comp = new Component();
+	
+	comp.added = function(e){
+		e.remove = function(){
+			var parent = e.parent;
+			e.parent.removeChild(e);
+			if (_.isFunction(onRemoved)){
+				onRemoved(e, parent);
+			}
 		}
 	}
 	
-	bloon.tween = createjs.Tween.get(bloon).to({
-		y: $canvas.height()-bloon.height
-	}, bloon.v, createjs.Ease.linear)
-	.call(function(tween){
-		tween._target.adios();
-	});
+	return comp;
+}
+
+var Poppable = function(){
 	
-	var wave = _.random(-5*bloon.width, 5*bloon.width);
-	var origx = bloon.x;
-	bloon.wavyTween = createjs.Tween.get(bloon, {loop: true})
-		.to({x: bloon.x + wave}, _.random(1000,2000), createjs.Ease.sineInOut)
-		.to({ x: origx },_.random(1000,2000), createjs.Ease.sineInOut);
+	var comp = new Component();
 	
-	bloon.click = function(evt){
-		createjs.Sound.play("p" + _.sample([1,2,3,4,5]), createjs.Sound.INTERRUPT_NONE);
-		createjs.Tween.removeTweens(evt.target);
+	comp.added = function(e){
 		
-		evt.target.set(_.sample([{
-			skewX: _.sample([_.random(55,75),_.random(-75,-55)])
-		},
-		{
-			skewY: _.sample([_.random(55,75),_.random(-75,-55)])
-		}]));
-		
-		evt.target.removeAllEventListeners();
-		
-		var toX = _.sample([-3*evt.target.width, $canvas.width()+3*evt.target.width]);
-		var toY = _.random(-3*evt.target.height, $canvas.height()+3*evt.target.height);
-		createjs.Tween.get(evt.target).to({y: toY}, 500, createjs.Ease.elasticInOut);
-		createjs.Tween.get(evt.target).to({x: toX}, 500, createjs.Ease.linear).call(function(tween){
-			evt.target.adios();
-		});
-		//evt.target.adios();
-		//Add the popped bitmap, or better yet play some animation thing?
-	};
-	
-	bloon.addEventListener("click", bloon.click);
-	
-	switch(type)
-	{
-		case BLOON_TYPE.NORMAL:
+		e.on("click", function(evt){
 			
-			return bloon;
-			break;
-		default:
-			throw "We don't make type " + type + " bloons here. Find another factory!";
+			createjs.Sound.play("p" + _.sample([1,2,3,4,5]), createjs.Sound.INTERRUPT_NONE);
+			createjs.Tween.removeTweens(evt.target);
+			evt.target.set(_.sample([{
+				skewX: _.sample([_.random(55,75),_.random(-75,-55)])
+			},
+			{
+				skewY: _.sample([_.random(55,75),_.random(-75,-55)])
+			}]));
+			
+			evt.target.removeAllEventListeners();
+			
+			var toX = _.sample([-3*evt.target.width, $canvas.width()+3*evt.target.width]);
+			var toY = _.random(-3*evt.target.height, $canvas.height()+3*evt.target.height);
+			
+			createjs.Tween.get(evt.target).to({y: toY}, 500, createjs.Ease.elasticInOut);
+			
+			createjs.Tween.get(evt.target).to({x: toX}, 500, createjs.Ease.linear).call(function(tween){
+				evt.target.remove();
+			});
+			
+		});
+		
 	}
+	
+	return comp;
+}
+
+var Timer = function(name, options, callback){
+	
+	var comp = new Component();
+	
+	comp.added = function(e){
+	
+		e.timers = e.timers || {};
+		options = _.extend({duration: 1000, paused: false, loop: false},options);
+		e.timers[name] = options;
+		
+		var origDuration = options.duration;
+		
+		createjs.Ticker.on("tick",function(evt){
+			//Should probably make this remove the tick event on destruction somehow.
+			//console.log(evt);
+			//if (!e.timers[name])
+			//	return;
+				
+			if (!e.timers[name].paused)
+				e.timers[name].duration -= evt.delta;
+			
+			if (e.timers[name].duration <= 0){
+				e.timers[name].duration = 0;
+				callback(name, e);
+				if (e.timers[name].loop){
+					e.timers[name].duration = origDuration;
+				}
+			}
+				
+		});
+		
+	}
+	
+	return comp;
+	
+}
+
+//Factories
+var Bloon = function(container){
+	
+	var bloon = new Entity();
+	
+	bloon.addComponent(new Bitmap(queue.getResult("bloon")));
+	bloon.addComponent(new Position(_.random(0, $canvas.width()-bloon.width), _.random(-$canvas.height(), 0-bloon.height)));
+	bloon.addComponent(new RandomColor());
+	bloon.addComponent(new SwayHorizontal(_.random(1000,2000)));
+	bloon.addComponent(new AnimateDown(_.random(10000,15000),$canvas.height()+bloon.height,function(tween){
+		bloon.remove();
+	}));
+	bloon.addComponent(new Removeable(function(e, parent){
+		bloons = _.without(bloons, e);
+		if (bloons.length < MAX_BLOONS){
+			var newBloon = new Bloon(parent);
+			newBloon.SwayHorizontal.setPaused(false);
+			newBloon.AnimateDown.setPaused(false);
+			bloons.push(newBloon);
+			parent.addChild(newBloon);
+		}
+	}));
+	bloon.addComponent(new Poppable());
+	
+	bloon.snapToPixel = true;
+	bloon.SwayHorizontal.setPaused(true);
+	bloon.AnimateDown.setPaused(true);
+	
+	return bloon;
+	
+}
+
+var MenuOption = function(text, font, color, x, y, onClick){
+
+	var menu = new Entity();
+	
+	menu.addComponent(new Text(text,font,color));
+	menu.addComponent(new Position(x, y, menu.getMeasuredWidth()/2, menu.getMeasuredHeight()/2));
+	menu.addComponent(new MouseOverFlipOut());
+	
+	if (_.isFunction(onClick)){
+		menu.on("click",onClick);
+	}
+	
+	return menu;
+	
 }
